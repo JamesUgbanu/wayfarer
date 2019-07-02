4a600dd1559996f849b5d81fc1af5df521b4102a
techneplus <jamesugbanu@gmail.com>
2019-07-02 14:40:59 +0100

    linted my codes

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ const app = new Express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
-    extended: false,
-  }));
-  app.use(bodyParser.json());
+  extended: false,
+}));
+app.use(bodyParser.json());
 
-  routes(app);
-  
+routes(app);
+
 // declare 404 route
 app.all('*', (req, res) => res.status(404).json({
-    status: 404,
-    error: 'The URL you are trying to access does not exist. Please enter a valid url',
-  }));
-  
-  // listen to app port
-  app.listen(port, () => console.log(`App listening on port ${port}`));
-  
-  export default app;
+  status: 404,
+  error: 'The URL you are trying to access does not exist. Please enter a valid url',
+}));
+
+// listen to app port
+app.listen(port, () => console.log(`App listening on port ${port}`));
+
+export default app;
