f1a3b04e50ff6d29f26b5fe3e455de45aa252a56
techneplus <jamesugbanu@gmail.com>
2019-07-03 16:41:12 +0100

    add signup function and authentication errors

---
+++ server/helpers/validationErrors.js
@@ -0,0 +1,6 @@
+const validationErrors = {
+  loginFailure: 'Could not login. The email or password is incorrect',
+  noEmail: 'Could not login. The email and password do not match',
+};
+
+export default validationErrors;
